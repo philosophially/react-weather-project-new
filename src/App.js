e0761f59747e820847be91c8c224323b73e0b0ad
sophia <sophialeeloves@gmail.com>
2025-02-20 02:10:26 +0700

    Reinstall React and remove unnecessary .js files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather App</h1>
+
+        <br />
+        <p>
+          🙌🏼 This project was coded by Sophia Lee and it is{" "}
+          <a
+            href="https://github.com/philosophially/weather-project-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+          👩🏼‍💻
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;

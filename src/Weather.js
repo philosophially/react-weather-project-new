760c1375c02ce542d87f6014c06499dc1fb0bac0
sophia <sophialeeloves@gmail.com>
2025-02-20 02:41:51 +0700

    Updated border and information for Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -3,20 +3,35 @@ import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div ClassName="Weather">
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <br />
       <ul>
         <li>Thursday 02:00</li>
         <li>Partly Cloudy</li>
       </ul>
-      <div ClassName="row">
-        <div ClassName="col-6">
+      <div className="row">
+        <div className="col-6">
           <img
             src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
             alt="Partly Cloudy"
           />
           26°C
         </div>
-        <div ClassName="col-6">
+        <div className="col-6">
           <ul>
             <li>Precipitation: 0%</li>
             <li>Humidity: 90%</li>

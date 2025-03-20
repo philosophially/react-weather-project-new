fa366c2b06a8bd3d43810e58c30ef2df61e2ca66
sophia <sophialeeloves@gmail.com>
2025-03-21 01:27:38 +0700

    Fixed error messages - Added data to all prop callings

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -3,17 +3,17 @@ import WeatherIconSheCodes from "./WeatherIconSheCodes";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
-    let temperature = Math.round(props.temperature.maximum);
+    let temperature = Math.round(props.data.temperature.maximum);
     return `${temperature}°`;
   }
 
   function minTemperature() {
-    let temperature = Math.round(props.temperature.minimum);
+    let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}°`;
   }
 
   function day() {
-    let date = new Date(props.time * 1000);
+    let date = new Date(props.data.time * 1000);
     let day = date.getDay();
 
     let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -24,7 +24,7 @@ export default function WeatherForecastDay(props) {
   return (
     <div>
       <div className="WeatherForecast-day">{day()}</div>
-      <WeatherIconSheCodes code={props.condition.icon} size={32} />
+      <WeatherIconSheCodes code={props.data.condition.icon} size={32} />
       <div className="WeatherForecast-temp">
         <span className="WeatherForecast-temp-maximum">{maxTemperature()}</span>
         <span className="WeatherForecast-temp-minimum">{minTemperature()}</span>
